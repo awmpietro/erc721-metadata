bf6d7510906f28d3ee4a399578f013526356adfb
Arthur Mastropietro <awmpietro@gmail.com>
2020-02-14 15:58:55 -0300

    Updates

---
+++ precache-manifest.6da3f885db72eea7dcef9a373f1b2079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42999c984549b1c93ee7f821beb73a05",
+    "url": "/erc721-metadata/index.html"
+  },
+  {
+    "revision": "93a4b5eee298210b9241",
+    "url": "/erc721-metadata/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "8bb4c34d160c35189ed5",
+    "url": "/erc721-metadata/static/css/main.85a1e678.chunk.css"
+  },
+  {
+    "revision": "93a4b5eee298210b9241",
+    "url": "/erc721-metadata/static/js/2.8b9b1a0e.chunk.js"
+  },
+  {
+    "revision": "ee9c07e13689b652de2667a98202d718",
+    "url": "/erc721-metadata/static/js/2.8b9b1a0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bb4c34d160c35189ed5",
+    "url": "/erc721-metadata/static/js/main.0ec8c062.chunk.js"
+  },
+  {
+    "revision": "b7ea9061ad74b33e7bee",
+    "url": "/erc721-metadata/static/js/runtime-main.8dcbdbef.js"
+  }
+]);
